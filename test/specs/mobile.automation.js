a496a2399ceb836fa354dc18686b440f67d5c24a
MuhammadZikri <zikrim577@gmail.com>
2023-07-09 13:56:07 +0700

    first commit

---
+++ test/specs/mobile.automation.js
@@ -0,0 +1,34 @@
+describe('Open Application', () => {
+    it('Input Form Sukses', async () => {
+        // mengisi form
+        const inputName = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.EditText');
+        const inputBerat = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[2]/android.widget.FrameLayout/android.widget.EditText');
+        const inputTinggi = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout[3]/android.widget.FrameLayout/android.widget.EditText');
+        const pilihJenisKelamin = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RadioGroup/android.widget.RadioButton[1]');
+        const buttonNext = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button');
+
+        // memilih aktivitas
+        const aktivitas = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.RadioGroup/android.widget.RadioButton[4]');
+        const buttonFinish = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.Button');
+
+        // memilih item card
+        const iconCard = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[1]/android.view.ViewGroup');
+
+        // memanggil variabel form yang ada diatas
+        await inputName.click();
+        await inputName.setValue('Alex');
+        await inputBerat.click();
+        await inputBerat.setValue('60');
+        await inputTinggi.click();
+        await inputTinggi.setValue('170');
+        await pilihJenisKelamin.click();
+        await buttonNext.click();
+
+        // memanggil variabel aktivitas yang ada diatas
+        await aktivitas.click();
+        await buttonFinish.click();
+
+        // memanggil item card yang ada diatas
+        await iconCard.click();
+    })
+})
